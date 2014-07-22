c1de7f93946f360f0cf2126373c14206f0ea842e
ml3613 <ml3613@doc.ic.ac.uk>
2014-07-22 16:30:32 +0100

    Website is not layed out and we can upload images to the VM

--- website/hello.py
+++ website/hello.py
@@ -1,11 +1,19 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/home/matthew/Documents/classifier/website/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff'])
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.debug = True
 #@app.route('/')
 #def index():
 #    return 'Index Page'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def root():
@@ -14,7 +22,33 @@ def root():
 
 @app.route('/hello')
 def hello():
-    return 'Hello World'
+    return 'Hello World! This is a legacy page for testing.'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/me')
+def me():
+    return render_template('me.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/butterflies',methods=['GET','POST'])
+def butterflies():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',filename=filename))
+    return render_template('butterflies.html')
+
+@app.route('/wasps')
+def wasps():
+    return render_template('wasps.html')
 
 if __name__ == '__main__':
     app.run()
