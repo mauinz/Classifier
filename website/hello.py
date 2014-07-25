e71e4e8513fe3c6ee58ba9cf90bee90a76cbe49a
ml3613 <ml3613@doc.ic.ac.uk>
2014-07-25 15:17:23 +0100

    Website can now classify images and I neatened up the containers slightly

--- website/hello.py
+++ website/hello.py
@@ -1,9 +1,15 @@
 import os
+import classifier
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/matthew/Documents/classifier/website/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff'])
+SVM_PATH = '/home/matthew/Documents/classifier/Classifier/SVMS_12014-07-24.14:30:30'
+VOCAB_PATH = '/home/matthew/Documents/classifier/Classifier/Vocabularies/Vocabulary_1_2014-07-16.18:03:34.yml'
+ 
+
+#classifier.classify("/home/matthew/Documents/classifier/Classifier/SVMS_12014-07-24.14:30:30","/home/matthew/Documents/classifier/Classifier/Vocabularies/Vocabulary_1_2014-07-16.18:03:34.yml","/home/matthew/Documents/Data/UK-Leps.images/Aglais-urticae/Aglais-urticae-01.tif")
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -42,8 +48,12 @@ def butterflies():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',filename=filename))
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            filename = 'http://146.169.45.105/uploads/' + filename
+            res = classifier.classify(SVM_PATH,VOCAB_PATH, filepath)
+            #return redirect(url_for('uploaded_file',filename=filename))
+            return render_template('results.html',res=res,filename=filename)
     return render_template('butterflies.html')
 
 @app.route('/wasps')
